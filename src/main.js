baf8617aa2cbf5fa030348e3969e4fec37f92225
margallostephen <spmargallo@gmail.com>
2024-03-26 18:35:26 +0800

    Import router and global components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import router from './router'
+import TaskList from './components/TaskList.vue';
+import WebHeader from './components/WebHeader.vue';
 
-createApp(App).mount('#app')
+createApp(App).component('TaskList', TaskList).component('WebHeader', WebHeader).use(router).mount('#app');
